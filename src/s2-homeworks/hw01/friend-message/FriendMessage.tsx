7d232029ead62f63b2266cfed739c24952cce9eb
liranluxury <lirangraf@gmail.com>
2024-07-09 12:58:46 +0300

    homework HW2

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -10,18 +10,18 @@ const FriendMessage = (props: FriendMessageType) => {
     const { message } = props;
     return (
         <div
-            id={'hw1-friend-message-' + props.message.id}
+            id={'hw1-friend-message-' + message.id}
             className={s.friendMessage}
         >
             <div className={s.friendImageAndText}>
                 <div className={s.friendFotoWrapper} >
                     <img
-                        id={'hw1-friend-avatar-' + props.message.id}
+                        id={'hw1-friend-avatar-' + message.id}
                         src={message.user.avatar}
                         alt="friend avatar"
                     />
                     <div
-                        id={'hw1-friend-time-' + props.message.id}
+                        id={'hw1-friend-time-' + message.id}
                         className={s.friendTime}
                     >
                         <span>{message.message.time}</span>
@@ -29,13 +29,13 @@ const FriendMessage = (props: FriendMessageType) => {
                 </div>
                 <div className={s.friendText}>
                     <div
-                        id={'hw1-friend-name-' + props.message.id}
+                        id={'hw1-friend-name-' + message.id}
                         className={s.friendName}
                     >
                         <span>{message.user.name}</span>
                     </div>
                     <pre
-                        id={'hw1-friend-text-' + props.message.id}
+                        id={'hw1-friend-text-' + message.id}
                         className={s.friendMessageText}
                     >
                         <span>{message.message.text}</span>
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -11,25 +11,25 @@ export type MessagePropsType = {
 const Message = (props: MessagePropsType) => {
     const { message } = props;
     return (
-        <div id={'hw1-message-' + props.message.id} className={s.message}>
+        <div id={'hw1-message-' + message.id} className={s.message}>
             <div className={s.imageAndText}>
                 <div className={s.fotoWrapper}>
 
                     <img
-                    id={'hw1-avatar-' + props.message.id}
+                    id={'hw1-avatar-' + message.id}
                     src={message.user.avatar}
                     alt="avatar"
                 />
-                    <div id={'hw1-time-' + props.message.id} className={s.time}>
+                    <div id={'hw1-time-' + message.id} className={s.time}>
                         <span>{message.message.time}</span>
                     </div>
 
                 </div>
                 <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
+                    <div id={'hw1-name-' + message.id} className={s.name}>
                         <span>{message.user.name}</span>
                     </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                    <pre id={'hw1-text-' + message.id} className={s.messageText}>
                        <span>{message.message.text}</span>
                     </pre>
                 </div>
